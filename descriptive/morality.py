aeeb2a7b597822aae7e01a9b5969ba0304286d63
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-06-19 19:28:13 +0200

    Plot waves together with morality classes

--- descriptive/morality.py
+++ descriptive/morality.py
@@ -1,43 +1,35 @@
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from sklearn.semi_supervised import LabelPropagation
-import spacy
 from __init__ import *
 from simpletransformers.language_representation import RepresentationModel
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from sklearn.semi_supervised import LabelPropagation
 
 from preprocessing.constants import MORALITY_ENTITIES
-from preprocessing.embeddings import compute_embeddings
+from preprocessing.embeddings import compute_embeddings, transform_embeddings
 
 
 #Plot morality embeddings of all waves
-def plot_morality_embeddings(embeddings_file, model, morality_entities, label_propagation=False, dim_reduction='TSNE', perplexity=5):
+def plot_morality_embeddings(embeddings_file, model, anchors, label_propagation=False, dim_reduction='TSNE', perplexity=5):
 
     interviews = pd.read_pickle(embeddings_file)
-    interviews = interviews[['Wave', 'R:Morality:M1_Embeddings']].dropna()
-    interviews.columns = ['Name', 'Embeddings']
+    interviews = interviews[['Wave', 'R:Morality_Embeddings']].dropna().rename(columns={'Wave': 'Name', 'R:Morality_Embeddings': 'Embeddings'})
     interviews['Name'] = interviews['Name'].apply(lambda x: 'Wave ' + str(x))
     
-    if model == 'trf':
-        transformer = RepresentationModel(model_type='bert', model_name='bert-base-uncased', use_cuda=False)
-        vectorizer = lambda x: pd.Series(transformer.encode_sentences(x, combine_strategy='mean').tolist())
-    elif model in ['lg', 'md']:
-        nlp = spacy.load('en_core_web_lg')
-        vectorizer = lambda x: x.apply(lambda y: nlp(y).vector)
+    interviews['Embeddings'], anchors = transform_embeddings(interviews['Embeddings'], anchors, model)
     
-    morality_entities = pd.DataFrame(morality_entities).melt(var_name='Name', value_name='Embeddings')
-    morality_entities['Embeddings'] = vectorizer(morality_entities['Embeddings'].str.lower())
     # morality_entities = morality_entities.groupby('Name').mean().reset_index()
     
     if label_propagation:
         label_propagation = LabelPropagation()
-        label_propagation.fit(morality_entities['Embeddings'].apply(pd.Series), morality_entities['Name'])
+        label_propagation.fit(anchors['Embeddings'].apply(pd.Series), anchors['Name'])
         interviews['Name'] = interviews['Name'].str.cat(label_propagation.predict(interviews['Embeddings'].apply(pd.Series)), sep=' - ')
         data = interviews
     else:
-        data = pd.concat([interviews, morality_entities], ignore_index=True)
+        data = pd.concat([interviews, anchors], ignore_index=True)
 
     if dim_reduction == 'TSNE':
         tsne = TSNE(n_components=2, perplexity=perplexity, random_state=42)
@@ -45,19 +37,25 @@ def plot_morality_embeddings(embeddings_file, model, morality_entities, label_pr
 
     elif dim_reduction == 'PCA':
         pca = PCA(n_components=2, random_state=42)
-        pca.fit(morality_entities['Embeddings'].apply(pd.Series))
+        pca.fit(anchors['Embeddings'].apply(pd.Series))
         data = data[['Name']].join(pd.DataFrame(pca.transform(data['Embeddings'].apply(pd.Series))))
 
     sns.set(context='paper', style='white', color_codes=True, font_scale=4)
     plt.figure(figsize=(20, 20))
+    color_palette = sns.color_palette('colorblind')
+    
+    sns.kdeplot(data=data[data['Name'].isin(['Wave 1', 'Wave 2', 'Wave 3'])], x=0, y=1, hue='Name', fill=True, alpha=0.5, thresh=.4, hue_order=['Wave 1', 'Wave 2', 'Wave 3'], palette=color_palette[:3])
+    sns.scatterplot(data=data[~data['Name'].isin(['Wave 1', 'Wave 2', 'Wave 3'])], x=0, y=1, hue='Name', palette=color_palette[3:], s = 500)
 
-    ax = sns.scatterplot(data=data, x=0, y=1, hue='Name', palette='Set2', s = 500)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    for p in [patches.Patch(color=color_palette[0], label='Wave 1'), patches.Patch(color=color_palette[1], label='Wave 2'), patches.Patch(color=color_palette[2], label='Wave 3')]:
+        handles.append(p)
+        labels.append(p.get_label())
 
-    plt.legend(loc='upper left', markerscale=5, bbox_to_anchor=(1, 1))
+    plt.legend(handles, labels, loc='upper left', markerscale=5, bbox_to_anchor=(1, 1), title='')
     plt.xlabel('')
     plt.ylabel('')
     plt.title('Morality Embeddings')
-    plt.gca().get_legend().set_title('')
     plt.savefig('data/plots/morality_embeddings.png', bbox_inches='tight')
     plt.show()
 
@@ -66,4 +64,4 @@ def plot_morality_embeddings(embeddings_file, model, morality_entities, label_pr
 if __name__ == '__main__':
     model = 'lg'
     # compute_embeddings('data/waves', 'data/cache/morality_embeddings_'+model+'.pkl', model=model, section='R:Morality')
-    plot_morality_embeddings(embeddings_file='data/cache/morality_embeddings_'+model+'.pkl', model=model, morality_entities=MORALITY_ENTITIES, label_propagation=True, dim_reduction='TSNE', perplexity=100)
+    plot_morality_embeddings(embeddings_file='data/cache/morality_embeddings_'+model+'.pkl', model=model, anchors=MORALITY_ENTITIES, label_propagation=False, dim_reduction='TSNE', perplexity=5)

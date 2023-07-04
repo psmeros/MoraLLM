bc6ae7c0f417bf64b311c3e8d382eb18cb0bece9
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-07-04 16:36:25 +0200

    Transform and plot morality embeddings

--- descriptive/morality.py
+++ descriptive/morality.py
@@ -1,59 +1,44 @@
-import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from __init__ import *
-from simpletransformers.language_representation import RepresentationModel
+from scipy.spatial.distance import cosine
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.semi_supervised import LabelPropagation
-from scipy.spatial.distance import cosine
 
-from preprocessing.constants import MORALITY_ENTITIES
 from preprocessing.embeddings import compute_embeddings, transform_embeddings
 
 
 #Plot morality embeddings of all waves
-def plot_morality_embeddings(embeddings_file, model, anchors, label_propagation=False, dim_reduction='TSNE', perplexity=5):
+def plot_morality_embeddings(embeddings_file, moral_foundations_file, model, label_propagation=False, dim_reduction='TSNE', perplexity=5):
 
     interviews = pd.read_pickle(embeddings_file)
     interviews = interviews[['Wave', 'R:Morality_Embeddings']].dropna().rename(columns={'Wave': 'Name', 'R:Morality_Embeddings': 'Embeddings'})
     interviews['Name'] = interviews['Name'].apply(lambda x: 'Wave ' + str(x))
     
-    interviews['Embeddings'], anchors = transform_embeddings(interviews['Embeddings'], anchors, model)
-    
-    # morality_entities = morality_entities.groupby('Name').mean().reset_index()
-    
+    moral_foundations = pd.read_pickle(moral_foundations_file)
+    interviews['Embeddings'] = transform_embeddings(interviews['Embeddings'], moral_foundations, model=model)
+        
     if label_propagation:
         label_propagation = LabelPropagation()
-        label_propagation.fit(anchors['Embeddings'].apply(pd.Series), anchors['Name'])
+        label_propagation.fit(moral_foundations['Embeddings'].apply(pd.Series), moral_foundations['Name'])
         interviews['Name'] = interviews['Name'].str.cat(label_propagation.predict(interviews['Embeddings'].apply(pd.Series)), sep=' - ')
-        data = interviews
-    else:
-        data = pd.concat([interviews, anchors], ignore_index=True)
 
-    if dim_reduction == 'TSNE':
-        tsne = TSNE(n_components=2, perplexity=perplexity, random_state=42)
-        data = data[['Name']].join(pd.DataFrame(tsne.fit_transform(data['Embeddings'].apply(pd.Series))))
+    #Dimensionality reduction
+    data = interviews
+    dim_reduction_alg = TSNE(n_components=2, perplexity=perplexity, random_state=42) if dim_reduction == 'TSNE' else PCA(n_components=2, random_state=42) if dim_reduction == 'PCA' else None
+    data = data[['Name']].join(pd.DataFrame(dim_reduction_alg.fit_transform(data['Embeddings'].apply(pd.Series))))
+    data = data.dropna()
 
-    elif dim_reduction == 'PCA':
-        pca = PCA(n_components=2, random_state=42)
-        pca.fit(anchors['Embeddings'].apply(pd.Series))
-        data = data[['Name']].join(pd.DataFrame(pca.transform(data['Embeddings'].apply(pd.Series))))
-
+    #Plot
     sns.set(context='paper', style='white', color_codes=True, font_scale=4)
     plt.figure(figsize=(20, 20))
-    color_palette = sns.color_palette('colorblind')
+    color_palette = sns.color_palette('Set2')
     
-    sns.kdeplot(data=data[data['Name'].isin(['Wave 1', 'Wave 2', 'Wave 3'])], x=0, y=1, hue='Name', fill=True, alpha=0.5, thresh=.4, hue_order=['Wave 1', 'Wave 2', 'Wave 3'], palette=color_palette[:3])
-    sns.scatterplot(data=data[~data['Name'].isin(['Wave 1', 'Wave 2', 'Wave 3'])], x=0, y=1, hue='Name', palette=color_palette[3:], s = 500)
+    waves = ['Wave 1', 'Wave 2', 'Wave 3']
+    sns.kdeplot(data=data[data['Name'].isin(waves)], x=0, y=1, hue='Name', fill=True, alpha=0.5, hue_order=waves, palette=color_palette[:3])
 
-    handles, labels = plt.gca().get_legend_handles_labels()
-    for p in [patches.Patch(color=color_palette[0], label='Wave 1'), patches.Patch(color=color_palette[1], label='Wave 2'), patches.Patch(color=color_palette[2], label='Wave 3')]:
-        handles.append(p)
-        labels.append(p.get_label())
-
-    plt.legend(handles, labels, loc='upper left', markerscale=5, bbox_to_anchor=(1, 1), title='')
     plt.xlabel('')
     plt.ylabel('')
     plt.title('Morality Embeddings')
@@ -61,12 +46,12 @@ def plot_morality_embeddings(embeddings_file, model, anchors, label_propagation=
     plt.show()
 
 #Plot moral foundations by wave
-def plot_moral_foundations(interviews_file, foundations_file):
+def plot_moral_foundations(embeddings_file, moral_foundations_file):
     #load data
-    interviews = pd.read_pickle(interviews_file)
+    interviews = pd.read_pickle(embeddings_file)
     interviews = interviews[['Wave', 'R:Morality_Embeddings']].dropna().rename(columns={'Wave': 'Name', 'R:Morality_Embeddings': 'Embeddings'})
     interviews['Name'] = interviews['Name'].apply(lambda x: 'Wave ' + str(x))
-    moral_foundations = pd.read_pickle(foundations_file)
+    moral_foundations = pd.read_pickle(moral_foundations_file)
     
     #compute similarity
     interviews = interviews.merge(moral_foundations, how='cross')
@@ -89,8 +74,16 @@ def plot_moral_foundations(interviews_file, foundations_file):
     
 
 if __name__ == '__main__':
-    model = 'lg'
-    compute_embeddings('data/waves', 'data/cache/morality_embeddings_POS_'+model+'.pkl', model=model, section='R:Morality', keep_POS=True)
-    # plot_morality_embeddings(embeddings_file='data/cache/morality_embeddings_'+model+'.pkl', model=model, anchors=MORALITY_ENTITIES, label_propagation=False, dim_reduction='TSNE', perplexity=5)
-    plot_moral_foundations(interviews_file='data/cache/morality_embeddings_POS_'+model+'.pkl', foundations_file='data/cache/moral_foundations.pkl')
+    model='lg'
+    embeddings_file='data/cache/morality_embeddings_POS_'+model+'.pkl'
+    moral_foundations_file='data/cache/moral_foundations.pkl'
+    label_propagation=False
+    dim_reduction='TSNE'
+    perplexity=5
+    plot_morality_embeddings(embeddings_file=embeddings_file, moral_foundations_file=moral_foundations_file, model=model, label_propagation=label_propagation, dim_reduction=dim_reduction, perplexity=perplexity)
+
+    model='lg'
+    embeddings_file='data/cache/morality_embeddings_POS_'+model+'.pkl'
+    moral_foundations_file='data/cache/moral_foundations.pkl'
+    plot_moral_foundations(embeddings_file=embeddings_file, moral_foundations_file=moral_foundations_file)
 
--- preprocessing/embeddings.py
+++ preprocessing/embeddings.py
@@ -8,21 +8,24 @@ from sklearn.linear_model import LinearRegression
 
 from preprocessing.transcript_parser import wave_parser
 
-def get_vectorizer(model='lg'):
+def get_vectorizer(model='lg', parallel=False):
     if model == 'trf':
         transformer = RepresentationModel(model_type='bert', model_name='bert-base-uncased', use_cuda=False)
         vectorizer = lambda x: pd.Series(transformer.encode_sentences(x, combine_strategy='mean').tolist())
     elif model in ['lg', 'md']:
         nlp = spacy.load('en_core_web_'+model)
-        pandarallel.initialize()
-        vectorizer = lambda x: x.parallel_apply(lambda y: nlp(y).vector)
+        if parallel:
+            pandarallel.initialize()
+            vectorizer = lambda x: x.parallel_apply(lambda y: nlp(y).vector)
+        else:
+            vectorizer = lambda x: x.apply(lambda y: nlp(y).vector)
     return vectorizer
 
 #Compute embeddings for a folder of transcripts
 def compute_embeddings(wave_folder, output_file, section, morality_breakdown=False, keep_POS=True, model='lg'):
     interviews = wave_parser(wave_folder, morality_breakdown=morality_breakdown)
 
-    #Keep only POS of interest (was ['VERB', 'NOUN', 'ADJ', 'ADV'], now just ['NOUN'])
+    #Keep only POS of interest
     if keep_POS:
         nlp = spacy.load('en_core_web_lg')
         pandarallel.initialize()
@@ -30,7 +33,7 @@ def compute_embeddings(wave_folder, output_file, section, morality_breakdown=Fal
 
     #Compute embeddings
     interviews = interviews.dropna(subset=[section])
-    vectorizer = get_vectorizer(model=model)
+    vectorizer = get_vectorizer(model=model, parallel=True)
     interviews[section + '_Embeddings'] = vectorizer(interviews[section])
     
     #Drop interviews with no embeddings
@@ -40,33 +43,28 @@ def compute_embeddings(wave_folder, output_file, section, morality_breakdown=Fal
     interviews.to_pickle(output_file)
 
 #Transform embeddings to match anchor embeddings
-def transform_embeddings(embeddings, anchors, model='lg'):
+def transform_embeddings(embeddings, moral_foundations, model='lg'):
 
-    #Compute anchor embeddings
-    anchors = pd.DataFrame(anchors).melt(var_name='Name', value_name='Embeddings')
+    #Average Vice and Virtue embeddings
+    moral_foundations['Name'] = moral_foundations['Name'].apply(lambda x: x.split('.')[0].capitalize())
+    moral_foundations = moral_foundations.groupby('Name').mean().reset_index()
+
+    #Compute old embeddings
     vectorizer = get_vectorizer(model=model)
-    anchors['Embeddings'] = vectorizer(anchors['Embeddings'].str.lower())
-    anchors = anchors.groupby('Name').mean().reset_index()
+    moral_foundations['Old Embeddings'] = vectorizer(moral_foundations['Name'].str.lower())
 
-    #Normalize anchor embeddings
-    np.random.seed(42)
-    anchors['Normalized Embeddings'] = [np.random.randn(anchors['Embeddings'].iloc[0].shape[0]) for _ in range(len(anchors))]
-    displacement_vector = anchors['Normalized Embeddings'].apply(pd.Series).to_numpy() - np.array(np.mean(anchors['Normalized Embeddings'], axis=0))
-    anchors['Normalized Embeddings'] = (displacement_vector / np.linalg.norm(displacement_vector)).tolist()
-
-    # Find transformation matrix
+    #Find transformation matrix
     regressor = LinearRegression()
-    regressor.fit(anchors['Embeddings'].apply(pd.Series), anchors['Normalized Embeddings'].apply(pd.Series))
+    regressor.fit(moral_foundations['Old Embeddings'].apply(pd.Series), moral_foundations['Embeddings'].apply(pd.Series))
     transformation_matrix = regressor.coef_.T
 
     #Transform embeddings
     embeddings = embeddings.apply(pd.Series).apply(lambda x: np.dot(x, transformation_matrix), axis=1)
-    anchors = anchors.drop(columns=['Embeddings']).rename(columns={'Normalized Embeddings': 'Embeddings'})
 
-    return embeddings, anchors
+    return embeddings
 
 #Compute morality anchors based on morality dictionary
-def compute_moral_foundations(dictionary_file, output_file):
+def compute_eMFD_embeddings(dictionary_file, output_file):
     dictionary = pd.DataFrame(pd.read_pickle(dictionary_file)).T
     dictionary = dictionary.reset_index(names=['word'])
 
@@ -86,9 +84,14 @@ def compute_moral_foundations(dictionary_file, output_file):
 
     moral_foundations.to_pickle(output_file)
 
+
 if __name__ == '__main__':
+    wave_folder = 'data/waves'
     model = 'lg'
-    # compute_embeddings(wave_folder='data/waves', output_file='data/cache/morality_embeddings_'+model+'.pkl', model=model, section='R:Morality', morality_breakdown=False, keep_POS=True)
-    compute_moral_foundations(dictionary_file='data/misc/eMFD.pkl', output_file='data/cache/moral_foundations.pkl')
+    section = 'R:Morality'
+    output_file = 'data/cache/morality_embeddings_POS_'+model+'.pkl'
+    compute_embeddings(wave_folder=wave_folder, output_file=output_file, model=model, section=section)
 
-
+    dictionary_file = 'data/misc/eMFD.pkl'
+    output_file = 'data/cache/moral_foundations.pkl'
+    compute_eMFD_embeddings(dictionary_file=dictionary_file, output_file=output_file)

45ea8f051fb7075370431db23f6c3cf77829733a
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-08-17 15:25:04 +0300

    Computed weighted average

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -13,18 +13,24 @@ def cross_entropy_loss(interviews):
     interviews[[mo + '_y' for mo in MORALITY_ORIGIN]] = interviews[[mo + '_y' for mo in MORALITY_ORIGIN]].applymap(int)
     
     #Compute loss
+    weight = interviews[[mo + '_y' for mo in MORALITY_ORIGIN]].sum()/interviews[[mo + '_y' for mo in MORALITY_ORIGIN]].sum().sum()
     loss = pd.Series({mo: log_loss(interviews[mo + '_y'], interviews[mo + '_x']) for mo in MORALITY_ORIGIN})
+    loss = (weight.reset_index(drop=True) * loss.reset_index(drop=True))
+    loss.index = MORALITY_ORIGIN
+    
     return loss
 
 
 if __name__ == '__main__':
     #Hyperparameters
     config = [1]
-    models = ['md', 'lg', 'entail']
+    models = ['md', 'lg', 'bert', 'bart', 'entail']
 
     for c in config:
         if c == 1:
             for model in models:
                 interviews = pd.read_pickle('data/cache/morality_embeddings_'+model+'.pkl')
                 loss = cross_entropy_loss(interviews)
-                print(model, 'model mean loss:', loss.mean())
+                print(model, 'model loss:', loss.sum())
+                # print(model, 'max loss:', loss.max(), loss.idxmax())
+                # print(model, 'max loss:', loss.min(), loss.idxmin())

0ce1d261bf590b6d5b18515e76254928ee6fc163
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-08-21 07:36:49 +0300

    Explain entailment

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from __init__ import *
 from sklearn.metrics import log_loss
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers_interpret import ZeroShotClassificationExplainer
 
 from preprocessing.constants import MORALITY_ORIGIN
 from preprocessing.metadata_parser import merge_codings
@@ -20,10 +22,24 @@ def cross_entropy_loss(interviews):
     
     return loss
 
+#Explain word-level attention for zero-shot models
+def explain_entailment(interviews):
+
+    pairs = [(interviews.iloc[interviews[mo + '_x'].idxmax()]['Morality_Origin'], [mo]) for mo in MORALITY_ORIGIN]
+
+    model_name = 'cross-encoder/nli-deberta-base'
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    zero_shot_explainer = ZeroShotClassificationExplainer(model, tokenizer)
+
+    for text, labels in pairs:
+        zero_shot_explainer(text=text, hypothesis_template='The morality origin is {}.',labels=labels)
+        zero_shot_explainer.visualize('data/misc/zero_shot.html')
 
 if __name__ == '__main__':
     #Hyperparameters
-    config = [1]
+    config = [2]
     models = ['md', 'lg', 'bert', 'bart', 'entail']
 
     for c in config:
@@ -34,3 +50,9 @@ if __name__ == '__main__':
                 print(model, 'model loss:', loss.sum())
                 # print(model, 'max loss:', loss.max(), loss.idxmax())
                 # print(model, 'max loss:', loss.min(), loss.idxmin())
+        if c == 2:
+            interviews = pd.read_pickle('data/cache/morality_embeddings_entail.pkl')
+            interviews = interviews[interviews['Wave'] == 1]
+            interviews = merge_codings(interviews)
+            explain_entailment(interviews)
+

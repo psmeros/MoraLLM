743a0341008e090518ef25536a2e8cc8142b25f3
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-09-19 01:15:54 +0200

    Plot cross-entropy loss for all models

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -10,24 +10,51 @@ from transformers_interpret import ZeroShotClassificationExplainer
 from preprocessing.constants import MORALITY_ORIGIN, CODERS
 from preprocessing.metadata_parser import merge_codings
 
+#Plot cross-entropy loss for all models
+def plot_cross_entropy_loss(models = ['lg', 'bert', 'bart', 'entail']):
+    #Compute losses
+    losses = []
+    for model in models:
+        interviews = pd.read_pickle('data/cache/morality_embeddings_'+model+'.pkl')
+        interviews = interviews[interviews['Wave'].isin([1,3])]
+        interviews = merge_codings(interviews)
 
-def cross_entropy_loss(interviews):
-    #Keep interviews with codings and merge 
-    interviews = interviews[interviews['Wave'].isin([1,3])]
-    interviews = merge_codings(interviews)
+        for mo in MORALITY_ORIGIN:
+            interviews[mo + '_All Codings'] = interviews[mo + '_' + CODERS[0]] | interviews[mo + '_' + CODERS[1]]
+            interviews[mo + '_Common Codings'] = interviews[mo + '_' + CODERS[0]] & interviews[mo + '_' + CODERS[1]]
 
-    for coder in CODERS:
-        interviews[[mo + '_' + coder for mo in MORALITY_ORIGIN]] = interviews[[mo + '_' + coder for mo in MORALITY_ORIGIN]].applymap(int)
-        
-        #Compute loss
-        weight = interviews[[mo + '_' + coder for mo in MORALITY_ORIGIN]].sum()/interviews[[mo + '_' + coder for mo in MORALITY_ORIGIN]].sum().sum()
-        loss = pd.Series({mo: log_loss(interviews[mo + '_' + coder], interviews[mo]) for mo in MORALITY_ORIGIN})
-        loss = (weight.reset_index(drop=True) * loss.reset_index(drop=True))
-        loss.index = MORALITY_ORIGIN
-        
-        print('Coder:', coder, 'Model:', model, 'Loss:', loss.sum())
-        # print('Max Loss:', loss.max(), loss.idxmax())
-        # print('Min Loss:', loss.min(), loss.idxmin())
+        for c in ['All Codings', CODERS[1], CODERS[0], 'Common Codings']:
+            losses.append({'Model': model, 'Codings': c, 'Loss': pd.Series({mo: log_loss(interviews[mo + '_' + c].astype(int), interviews[mo]) for mo in MORALITY_ORIGIN}).mean()})
+    losses = pd.DataFrame(losses)
+    baseline_loss = pd.Series({mo: log_loss(interviews[mo + '_' + CODERS[1]].astype(int), interviews[mo + '_' + CODERS[0]].astype(int)) for mo in MORALITY_ORIGIN}).sort_values()
+
+    #Plot model comparison
+    sns.set(context='paper', style='white', color_codes=True, font_scale=4)
+    plt.figure(figsize=(10, 10))
+    ax = sns.barplot(losses, x = 'Loss', y = 'Model', hue='Codings', palette='Set2')
+    plt.axvline(x=baseline_loss.mean(), linestyle='--', linewidth=4, label='Coders Agreement')
+    plt.axvline(x=0.4, linestyle='--', linewidth=2, color='grey')
+    plt.axvline(x=0.9, linestyle='--', linewidth=2, color='grey')
+    plt.xlabel('Cross-Entropy Loss')
+    plt.xscale('log')
+    plt.xticks([0.4, 0.9, 6], ['0.4', '0.9', '6'])
+    plt.xlim(0.1, 15)
+    plt.yticks([0, 1, 2, 3], ['SpaCy', 'Bert', 'Bart', 'Entailment'])
+    plt.title('Model Comparison')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.8, 1.03))
+    plt.savefig('data/plots/evaluation-model_comparison.png', bbox_inches='tight')
+    plt.show()
+
+    #Plot coders agreement
+    sns.set(context='paper', style='white', color_codes=True, font_scale=4)
+    plt.figure(figsize=(10, 10))
+    ax = sns.barplot(x = baseline_loss.values, y = baseline_loss.index, palette='Blues_d')
+    plt.xticks([0, 5, 10, 15])
+    plt.xlabel('Cross-Entropy Loss')
+    plt.ylabel('Morality Origin')
+    plt.title('Coders Agreement')
+    plt.savefig('data/plots/evaluation-coders_agreement.png', bbox_inches='tight')
+    plt.show()
 
 #Explain word-level attention for zero-shot models
 def explain_entailment(interviews):
@@ -76,13 +103,10 @@ def plot_morality_evolution(interviews):
 if __name__ == '__main__':
     #Hyperparameters
     config = [1]
-    models = ['md', 'lg', 'bert', 'bart', 'entail']
 
     for c in config:
         if c == 1:
-            for model in models:
-                interviews = pd.read_pickle('data/cache/morality_embeddings_'+model+'.pkl')
-                cross_entropy_loss(interviews)
+            plot_cross_entropy_loss()
         if c == 2:
             interviews = pd.read_pickle('data/cache/morality_embeddings_entail.pkl')
             interviews = interviews[interviews['Wave'] == 1]

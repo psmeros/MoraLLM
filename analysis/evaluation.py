088559fe50b7ba41dcd37c232c68b6a753a31790
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-09-21 02:10:30 +0200

    Plotted coders agreement

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from __init__ import *
-from sklearn.metrics import log_loss
+from sklearn.metrics import cohen_kappa_score, log_loss
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from transformers_interpret import ZeroShotClassificationExplainer
 
-from preprocessing.constants import MORALITY_ORIGIN, CODERS
+from preprocessing.constants import CODERS, MORALITY_ORIGIN
 from preprocessing.metadata_parser import merge_codings
 
 #Plot cross-entropy loss for all models
@@ -41,12 +42,15 @@ def plot_cross_entropy_loss(models = ['lg', 'bert', 'bart', 'entail']):
     sns.set(context='paper', style='white', color_codes=True, font_scale=4)
     plt.figure(figsize=(10, 10))
     ax = sns.barplot(losses, x = 'Loss', y = 'Model', hue='Codings', palette='Set2')
-    plt.axvline(x=baseline_loss.sum(), linestyle='--', linewidth=4, label='Coders Agreement')
-    plt.axvline(x=0.6, linestyle='--', linewidth=2, color='grey')
-    plt.axvline(x=1.2, linestyle='--', linewidth=2, color='grey')
+    line_1 = round(losses['Loss'].min(), 1)
+    line_2 = round(losses['Loss'].max(), 1)
+    line_3 = round(baseline_loss.sum(), 1)
+    plt.axvline(x=line_1, linestyle='--', linewidth=2, color='grey')
+    plt.axvline(x=line_2, linestyle='--', linewidth=2, color='grey')
+    plt.axvline(x=line_3, linestyle='--', linewidth=4, label='Coders Agreement')
     plt.xlabel('Weighted Cross-Entropy Loss')
     plt.xscale('log')
-    plt.xticks([0.6, 1.2, 7.4], ['0.6', '1.2', '7.4'])
+    plt.xticks([line_1, line_2, line_3], [str(line_1), str(line_2), str(line_3)])
     plt.xlim(0.1, 15)
     plt.yticks([0, 1, 2, 3], ['SpaCy', 'Bert', 'Bart', 'Entailment'])
     plt.title('Model Comparison')
@@ -65,9 +69,35 @@ def plot_cross_entropy_loss(models = ['lg', 'bert', 'bart', 'entail']):
     plt.savefig('data/plots/evaluation-coders_agreement.png', bbox_inches='tight')
     plt.show()
 
+#Plot coders agreement using Cohen's Kappa
+def plot_coders_agreement(interviews):
+    #Prepare heatmap
+    coder_0 = interviews[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].astype(int).values.T
+    coder_1 = interviews[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].astype(int).values.T
+    heatmap = np.zeros((len(MORALITY_ORIGIN), len(MORALITY_ORIGIN)))
+    
+    for mo_0 in range(len(MORALITY_ORIGIN)):
+        for mo_1 in range(len(MORALITY_ORIGIN)):
+            heatmap[mo_0, mo_1] = cohen_kappa_score(coder_0[mo_0], coder_1[mo_1])
+    heatmap = pd.DataFrame(heatmap, index=MORALITY_ORIGIN, columns=MORALITY_ORIGIN)
+
+    #Plot coders agreement
+    sns.set(context='paper', style='white', color_codes=True, font_scale=4)
+    plt.figure(figsize=(10, 10))
+    ax = sns.heatmap(heatmap, cmap = sns.color_palette('PuBuGn', n_colors=6), vmin=-0.2, vmax=1)
+    plt.ylabel(CODERS[0])
+    plt.xlabel(CODERS[1])
+    colorbar = ax.collections[0].colorbar
+    colorbar.set_ticks([n/10 for n in range(-1, 10, 2)]) 
+    colorbar.set_ticklabels(['Poor', 'Slight', 'Fair', 'Moderate', 'Substantial', 'Perfect'])
+
+    plt.title('Coders Agreement')
+    plt.savefig('data/plots/evaluation-coders_agreement.png', bbox_inches='tight')
+    plt.show()
+
+
 #Explain word-level attention for zero-shot models
 def explain_entailment(interviews):
-
     pairs = [(interviews.iloc[interviews[mo + '_x'].idxmax()]['Morality_Origin'], [mo]) for mo in MORALITY_ORIGIN]
 
     model_name = 'cross-encoder/nli-deberta-base'
@@ -111,16 +141,21 @@ def plot_morality_evolution(interviews):
 
 if __name__ == '__main__':
     #Hyperparameters
-    config = [1]
+    config = [2]
 
     for c in config:
         if c == 1:
             plot_cross_entropy_loss()
-        if c == 2:
+        elif c == 2:
+            interviews = pd.read_pickle('data/cache/morality_embeddings_entail.pkl')
+            interviews = interviews[interviews['Wave'].isin([1,3])]
+            interviews = merge_codings(interviews)
+            plot_coders_agreement(interviews)
+        elif c == 3:
             interviews = pd.read_pickle('data/cache/morality_embeddings_entail.pkl')
             interviews = interviews[interviews['Wave'] == 1]
             interviews = merge_codings(interviews)
             explain_entailment(interviews)
-        if c == 3:
+        elif c == 4:
             interviews = pd.read_pickle('data/cache/temporal_morality_embeddings_entail.pkl')
             plot_morality_evolution(interviews)

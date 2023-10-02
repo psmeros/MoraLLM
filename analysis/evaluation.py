f49f0ce023240f011b457f7cc23805cbc7d3ffaa
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-10-02 20:55:58 +0200

    Plot mean squared error breakdown + prior baseline

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from __init__ import *
-from sklearn.metrics import cohen_kappa_score, log_loss
+from sklearn.metrics import cohen_kappa_score, mean_squared_error
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from transformers_interpret import ZeroShotClassificationExplainer
 
@@ -12,74 +12,51 @@ from preprocessing.constants import CODERS, MORALITY_ORIGIN
 from preprocessing.metadata_parser import merge_codings
 
 
-#Plot cross-entropy loss for all models
-def plot_cross_entropy_loss(models = ['lg', 'bert', 'bart', 'entail']):
-    #Compute losses
-    losses = []
+#Plot mean-squared error for all models
+def plot_model_comparison(interviews, models = ['lg', 'bert', 'bart', 'entail']):
+
+    #Coders agreement
+    coders_agreement = pd.Series({mo:mean_squared_error(interviews[mo + '_' + CODERS[0]].astype(int), interviews[mo + '_' + CODERS[1]].astype(int)) for mo in MORALITY_ORIGIN}).sum()
+    golden_labels = interviews.apply(lambda i: pd.Series(i[mo + '_' + CODERS[0]] & i[mo + '_' + CODERS[1]] for mo in MORALITY_ORIGIN), axis=1).rename(columns={i:mo for i, mo in enumerate(MORALITY_ORIGIN)})
+    coder_A_loss = pd.Series({mo:mean_squared_error(golden_labels[mo].astype(int), interviews[mo + '_' + CODERS[0]].astype(int)) for mo in MORALITY_ORIGIN}).sum()
+    coder_B_loss = pd.Series({mo:mean_squared_error(golden_labels[mo].astype(int), interviews[mo + '_' + CODERS[1]].astype(int)) for mo in MORALITY_ORIGIN}).sum()
+    
+    #Baseline prior classifier
+    origin_prior = golden_labels.sum()/golden_labels.sum().sum()
+    prior_classifier = pd.DataFrame([origin_prior.tolist()] * len(interviews), columns=MORALITY_ORIGIN)
+        
+    #Compute mean squared error
+    loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo].astype(int), prior_classifier[mo]) for mo in MORALITY_ORIGIN}])
+    loss['Model'] = 'Baseline'
+    losses = [loss]
+
     for model in models:
         interviews = pd.read_pickle('data/cache/morality_embeddings_'+model+'.pkl')
         interviews = interviews[interviews['Wave'].isin([1,3])]
-        interviews = merge_codings(interviews)
 
-        for mo in MORALITY_ORIGIN:
-            interviews[mo + '_All Codings'] = interviews[mo + '_' + CODERS[0]] | interviews[mo + '_' + CODERS[1]]
-            interviews[mo + '_Common Codings'] = interviews[mo + '_' + CODERS[0]] & interviews[mo + '_' + CODERS[1]]
+        loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo].astype(int), interviews[mo]) for mo in MORALITY_ORIGIN}])
+        loss['Model'] = model
+        losses = [loss] + losses
 
-        weight = interviews[[mo + '_Common Codings' for mo in MORALITY_ORIGIN]].sum()/interviews[[mo + '_Common Codings' for mo in MORALITY_ORIGIN]].sum().sum()
-
-        for c in ['All Codings', CODERS[1], CODERS[0], 'Common Codings']:
-            loss = pd.Series({mo: log_loss(interviews[mo + '_' + c].astype(int), interviews[mo]) for mo in MORALITY_ORIGIN})
-            loss = weight.reset_index(drop=True) * loss.reset_index(drop=True)
-            losses.append({'Model': model, 'Codings': c, 'Loss': loss.sum()})
-        
-        if model == 'entail':
-            entailment_breakdown = []
-            for c in ['All Codings', CODERS[1], CODERS[0], 'Common Codings']:
-                loss = pd.Series({mo: log_loss(interviews[mo + '_' + c].astype(int), interviews[mo]) for mo in MORALITY_ORIGIN})
-                loss = weight.reset_index(drop=True) * loss.reset_index(drop=True)
-                loss.index = MORALITY_ORIGIN
-                loss['Codings'] = c
-                loss = pd.DataFrame(loss).T
-                entailment_breakdown = [loss] + entailment_breakdown
-            entailment_breakdown = pd.concat(entailment_breakdown, ignore_index=True)
-            
-    losses = pd.DataFrame(losses)
-
-    baseline_loss = pd.Series({mo: log_loss(interviews[mo + '_' + CODERS[1]].astype(int), interviews[mo + '_' + CODERS[0]].astype(int)) for mo in MORALITY_ORIGIN})
-    baseline_loss = weight.reset_index(drop=True) * baseline_loss.reset_index(drop=True)
-    baseline_loss.index = MORALITY_ORIGIN
-    baseline_loss = baseline_loss.sort_values()
+    losses = pd.concat(losses, ignore_index=True)
 
     #Plot model comparison
-    sns.set(context='paper', style='white', color_codes=True, font_scale=4)
+    sns.set(context='paper', style='white', color_codes=True, font_scale=2)
     plt.figure(figsize=(10, 10))
-    ax = sns.barplot(losses, x = 'Loss', y = 'Model', hue='Codings', palette='Set2')
-    line_1 = round(losses['Loss'].min(), 1)
-    line_2 = round(losses['Loss'].max(), 1)
-    line_3 = round(baseline_loss.sum(), 1)
-    plt.axvline(x=line_1, linestyle='--', linewidth=2, color='grey')
-    plt.axvline(x=line_2, linestyle='--', linewidth=2, color='grey')
-    plt.axvline(x=line_3, linestyle='--', linewidth=4, label='Coders Agreement')
-    plt.xlabel('Weighted Cross-Entropy Loss')
-    plt.xscale('log')
+    losses.plot(kind='barh', x = 'Model', stacked=True, colormap='Set2')
+    line_1 = round(coder_A_loss, 1)
+    line_2 = round(coder_B_loss, 1)
+    line_3 = round(coders_agreement, 1)
+    plt.axvline(x=line_1, linestyle='-.', linewidth=4, label='Coder A Error')
+    plt.axvline(x=line_2, linestyle=':', linewidth=4, label='Coder B Error')
+    plt.axvline(x=line_3, linestyle='-', linewidth=4, label='Coders Disagreement')
+    plt.xlabel('Mean Squared Error')
     plt.xticks([line_1, line_2, line_3], [str(line_1), str(line_2), str(line_3)])
-    plt.xlim(0.1, 15)
-    plt.yticks([0, 1, 2, 3], ['SpaCy', 'Bert', 'Bart', 'Entailment'])
+    plt.xlim(.0, 1.7)
+    plt.yticks([0, 1, 2, 3, 4], ['Entailment', 'Bart', 'Bert', 'SpaCy', 'Baseline'])
     plt.title('Model Comparison')
-    plt.legend(loc='upper right', bbox_to_anchor=(1.8, 1.03))
+    plt.legend(loc='upper right', bbox_to_anchor=(1.65, 1.03), fontsize='small')
     plt.savefig('data/plots/evaluation-model_comparison.png', bbox_inches='tight')
-    plt.show()
-
-    #Plot entailment model loss breakdown
-    sns.set(context='paper', style='white', color_codes=True, font_scale=4)
-    plt.figure(figsize=(10, 10))
-    entailment_breakdown.plot(kind='barh', x = 'Codings', stacked=True, colormap='Set2')
-    plt.xlabel('Cross-Entropy Loss')
-    plt.xticks([0, .5, 1])
-    plt.ylabel('Codings')
-    plt.title('Entailment Model Loss Breakdown')
-    plt.legend(loc='upper right', bbox_to_anchor=(1.8, 1.03), fontsize='xx-small')
-    plt.savefig('data/plots/evaluation-entailment_loss_breakdown.png', bbox_inches='tight')
     plt.show()
 
 #Plot coders agreement using Cohen's Kappa
@@ -161,7 +138,7 @@ if __name__ == '__main__':
 
     for c in config:
         if c == 1:
-            plot_cross_entropy_loss()
+            plot_model_comparison(interviews)
         elif c == 2:
             plot_coders_agreement(interviews)
         elif c == 3:

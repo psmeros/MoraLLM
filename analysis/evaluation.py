6a455bf671603ed9f0c68bd4c52bccd9d68ebe3a
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-10-12 15:50:07 +0200

    Homogenize evaluation

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -11,11 +11,11 @@ from preprocessing.metadata_parser import merge_codings
 
 
 #Plot mean-squared error for all models
-def plot_model_comparison(interviews, models):
+def plot_model_comparison(codings, models):
 
     #Compute golden labels
-    coder_A_labels = interviews[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].rename(columns={mo + '_' + CODERS[0]:mo for mo in MORALITY_ORIGIN})
-    coder_B_labels = interviews[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].rename(columns={mo + '_' + CODERS[1]:mo for mo in MORALITY_ORIGIN})
+    coder_A_labels = codings[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].rename(columns={mo + '_' + CODERS[0]:mo for mo in MORALITY_ORIGIN})
+    coder_B_labels = codings[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].rename(columns={mo + '_' + CODERS[1]:mo for mo in MORALITY_ORIGIN})
     golden_labels = coder_A_labels.astype(int) + coder_B_labels.astype(int)
     
     #Transform labels to probabilities
@@ -28,65 +28,58 @@ def plot_model_comparison(interviews, models):
     coder_A_loss = pd.Series({mo:mean_squared_error(golden_labels[mo], coder_A_labels[mo]) for mo in MORALITY_ORIGIN}).sum()
     coder_B_loss = pd.Series({mo:mean_squared_error(golden_labels[mo], coder_B_labels[mo]) for mo in MORALITY_ORIGIN}).sum()
 
-    #Baseline prior classifier
-    origin_prior = golden_labels.sum()/golden_labels.sum().sum()
-    prior_classifier = pd.DataFrame([origin_prior.tolist()] * len(interviews), columns=MORALITY_ORIGIN)
-        
-    #Compute mean squared error
-    loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo], prior_classifier[mo]) for mo in MORALITY_ORIGIN}])
-    loss['Model'] = 'Baseline (Prior)'
-    losses = [loss]
-
+    #Compute mean squared error for all models
+    losses = []
     for model in models:
-        interviews = pd.read_pickle('data/cache/morality_model-'+model+'.pkl')
+        if model == 'baseline':
+            origin_prior = golden_labels.sum()/golden_labels.sum().sum()
+            interviews = pd.DataFrame([origin_prior.tolist()] * len(codings), columns=MORALITY_ORIGIN).join(codings['Wave'])
+        elif model == 'min_max':
+            interviews = pd.read_pickle('data/cache/morality_model-entail.pkl')
+            interviews = pd.DataFrame(minmax_scale(interviews[MORALITY_ORIGIN]), columns=MORALITY_ORIGIN).join(interviews['Wave'])
+        elif model == 'one_hot':
+            interviews = pd.read_pickle('data/cache/morality_model-entail.pkl')
+            interviews = pd.DataFrame(interviews[MORALITY_ORIGIN].apply(lambda l: pd.Series({mo:1 if x == l.max() else 0 for mo,x in zip(MORALITY_ORIGIN, l)}), axis=1)).join(interviews['Wave'])
+        else:
+            interviews = pd.read_pickle('data/cache/morality_model-'+model+'.pkl')
+        
         interviews = interviews[interviews['Wave'].isin([1,3])]
-
         loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo], interviews[mo]) for mo in MORALITY_ORIGIN}])
         loss['Model'] = model
-        losses = [loss] + losses
+        losses.append(loss)
 
-    #Entailment alternatives
-    # min_max = pd.DataFrame(minmax_scale(interviews[MORALITY_ORIGIN]), columns=MORALITY_ORIGIN)
-    # loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo], min_max[mo]) for mo in MORALITY_ORIGIN}])
-    # loss['Model'] = 'Entailment (Min-Max)'
-    # losses.insert(1, loss)
-    # one_hot = interviews[MORALITY_ORIGIN].apply(lambda l: pd.Series({mo:1 if x == l.max() else 0 for mo,x in zip(MORALITY_ORIGIN, l)}), axis=1)
-    # loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo], one_hot[mo]) for mo in MORALITY_ORIGIN}])
-    # loss['Model'] = 'Entailment (One-Hot)'
-    # losses.insert(1, loss)
-
-    losses = pd.concat(losses, ignore_index=True)
-    losses['Model'] = losses['Model'].replace({'lg':'SpaCy (Labels)', 'bert':'BERT (Labels)', 'bart':'BART (Labels)', 'entail':'Entailment (Labels)', 'entail_explained':'Entailment (Labels+Notes)', 'top':'Entailment (Labels+Notes+Distro)', 'Baseline (Prior)':'Naive (Distro)'})
+    losses = pd.concat(losses, ignore_index=True).iloc[::-1]
+    losses['Model'] = losses['Model'].replace({'lg':'SpaCy (Labels)', 'bert':'BERT (Labels)', 'bart':'BART (Labels)', 'entail':'Entailment (Labels)', 'entail_explained':'Entailment (Labels+Notes)', 'top':'Entailment (Labels+Notes+Distro)', 'baseline':'Naive (Distro)'})
 
     #Plot model comparison
     sns.set(context='paper', style='white', color_codes=True, font_scale=2)
     plt.figure(figsize=(10, 10))
     losses.plot(kind='barh', x = 'Model', stacked=True, colormap='Set2')
-    line_1 = round(coder_A_loss, 1)
-    line_2 = round(coder_B_loss, 1)
-    line_3 = round(coders_agreement, 1)
-    min_loss = round(losses[MORALITY_ORIGIN].sum(axis=1).min(), 1)
-    max_loss = round(losses[MORALITY_ORIGIN].sum(axis=1).max(), 1)
+    line_1 = round(coder_A_loss, 2)
+    line_2 = round(coder_B_loss, 2)
+    line_3 = round(coders_agreement, 2)
+    min_loss = round(losses[MORALITY_ORIGIN].sum(axis=1).min(), 2)
     plt.axvline(x=line_1, linestyle='-.', linewidth=4, color='indianred', label='Coder A Error')
     plt.axvline(x=line_2, linestyle=':', linewidth=4, color='indianred', label='Coder B Error')
     plt.axvline(x=line_3, linestyle='-', linewidth=4, color='indianred', label='Coders Disagreement')
+    plt.axvline(x=min_loss, linestyle='--', linewidth=1, color='indianred')
     plt.xlabel('Mean Squared Error')
-    plt.xticks([line_1, line_2, line_3, min_loss, max_loss], [str(line_1), str(line_2), str(line_3), str(min_loss), str(max_loss)])
+    plt.xticks([line_1, line_2, line_3, min_loss], [str(line_1)[1:], str(line_2)[1:], str(line_3)[1:], str(min_loss)[1:]])
     plt.title('Model Comparison')
     plt.legend(loc='upper right', bbox_to_anchor=(1.65, 1.03), fontsize='small')
     plt.savefig('data/plots/evaluation-model_comparison.png', bbox_inches='tight')
     plt.show()
 
 #Plot coders agreement using Cohen's Kappa
-def plot_coders_agreement(interviews):
+def plot_coders_agreement(codings):
     #Prepare heatmap
-    coder_0 = interviews[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].astype(int).values.T
-    coder_1 = interviews[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].astype(int).values.T
+    coder_A = codings[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].astype(int).values.T
+    coder_B = codings[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].astype(int).values.T
     heatmap = np.zeros((len(MORALITY_ORIGIN), len(MORALITY_ORIGIN)))
     
-    for mo_0 in range(len(MORALITY_ORIGIN)):
+    for mo_A in range(len(MORALITY_ORIGIN)):
         for mo_1 in range(len(MORALITY_ORIGIN)):
-            heatmap[mo_0, mo_1] = cohen_kappa_score(coder_0[mo_0], coder_1[mo_1])
+            heatmap[mo_A, mo_1] = cohen_kappa_score(coder_A[mo_A], coder_B[mo_1])
     heatmap = pd.DataFrame(heatmap, index=MORALITY_ORIGIN, columns=MORALITY_ORIGIN)
 
     #Plot coders agreement
@@ -105,14 +98,14 @@ def plot_coders_agreement(interviews):
 
 if __name__ == '__main__':
     #Hyperparameters
-    config = [1,2]
-    models = ['lg', 'bert', 'bart', 'entail', 'entail_explained', 'top']
-    interviews = pd.read_pickle('data/cache/morality_model-top.pkl')
-    interviews = interviews[interviews['Wave'].isin([1,3])]
-    interviews = merge_codings(interviews)
+    config = [1]
+    models = ['baseline', 'lg', 'bert', 'bart', 'entail', 'entail_explained', 'top']
+    codings = pd.read_pickle('data/cache/morality_model-top.pkl')[['Wave', 'Interview Code']]
+    codings = codings[codings['Wave'].isin([1,3])]
+    codings = merge_codings(codings)
 
     for c in config:
         if c == 1:
-            plot_model_comparison(interviews, models)
+            plot_model_comparison(codings, models)
         elif c == 2:
-            plot_coders_agreement(interviews)
+            plot_coders_agreement(codings)

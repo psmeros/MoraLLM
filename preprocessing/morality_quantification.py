3f046f1fabe7ee9dbb5cc5aae1f62aaf7d1400ed
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2023-10-17 13:10:35 +0200

    Fixed aggregator from ChatGPT

--- preprocessing/morality_quantification.py
+++ preprocessing/morality_quantification.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import numpy as np
 import openai
@@ -174,7 +175,7 @@ def compute_morality_origin_model(interviews, model, section, dictionary_file='d
         openai.api_key = os.getenv('OPENAI_API_KEY')
         tokenizer = lambda text, token_limit=128: ' '.join(text.split(' ')[:token_limit])
         classifier = lambda text: openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=[{'role': 'system', 'content': CHATGPT_PROMPT},{'role': 'user','content': text}], temperature=0, max_tokens=64, top_p=1, frequency_penalty=0, presence_penalty=0)
-        aggregator = lambda response: pd.Series({i.split(':')[0]: float(i.split(':')[1]) for i in response['choices'][0]['message']['content'].split('\n')})
+        aggregator = lambda response: pd.Series({mo:float(re.search(mo+'.*:(.*?)(\n|$)', re.sub('100%', '1.0', response['choices'][0]['message']['content'])).group(1).strip()) if re.search(mo+'.*:(.*?)(\n|$)', response['choices'][0]['message']['content']) else 0.0 for mo in MORALITY_ORIGIN})
         full_pipeline = lambda text: aggregator(classifier(tokenizer(text)))
 
         #Classify morality origin and join results

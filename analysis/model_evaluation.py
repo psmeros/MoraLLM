5c58ef8c9be937532bbd1fca5ea99cb52b0be944
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2024-01-30 01:30:59 +0100

    Fixed model evaluation

--- analysis/model_evaluation.py
+++ analysis/model_evaluation.py
@@ -4,14 +4,13 @@ import pandas as pd
 import seaborn as sns
 from __init__ import *
 from sklearn.metrics import cohen_kappa_score, mean_squared_error
-from sklearn.preprocessing import minmax_scale
 
 from preprocessing.constants import CODERS, MERGE_MORALITY_ORIGINS, MORALITY_ORIGIN
 from preprocessing.metadata_parser import merge_codings
 
 
 #Plot mean-squared error for all models
-def plot_model_evaluation(codings, models, prefix):
+def plot_model_evaluation(codings, models):
     #Compute golden labels
     coder_A_labels = pd.DataFrame(codings[[mo + '_' + CODERS[0] for mo in MORALITY_ORIGIN]].values, columns=MORALITY_ORIGIN).astype(int).fillna(0)
     coder_B_labels = pd.DataFrame(codings[[mo + '_' + CODERS[1] for mo in MORALITY_ORIGIN]].values, columns=MORALITY_ORIGIN).astype(int).fillna(0)
@@ -26,25 +25,20 @@ def plot_model_evaluation(codings, models, prefix):
     #Compute mean squared error for all models
     losses = []
     for model in models:
-        if model == 'baseline':
-            origin_prior = golden_labels.sum()/golden_labels.sum().sum()
-            interviews = pd.DataFrame([origin_prior.tolist()] * len(codings), columns=MORALITY_ORIGIN).join(codings[['Wave', 'Interview Code']])
-        elif model == 'min_max':
-            interviews = pd.read_pickle('data/cache/'+prefix+'morality_model-entail.pkl')
-            interviews = pd.DataFrame(minmax_scale(interviews[MORALITY_ORIGIN]), columns=MORALITY_ORIGIN).join(interviews[['Wave', 'Interview Code']])
-        elif model == 'one_hot':
-            interviews = pd.read_pickle('data/cache/'+prefix+'morality_model-entail.pkl')
-            interviews = pd.DataFrame(interviews[MORALITY_ORIGIN].apply(lambda l: pd.Series({mo:1 if x == l.max() else 0 for mo,x in zip(MORALITY_ORIGIN, l)}), axis=1)).join(interviews[['Wave', 'Interview Code']])
-        else:
-            interviews = pd.read_pickle('data/cache/'+prefix+'morality_model-'+model+'.pkl')
-        
+        interviews = pd.read_pickle('data/cache/morality_model-'+model+'.pkl')
+        if model != 'top' and MERGE_MORALITY_ORIGINS:
+            interviews['Intuitive'] = interviews['Experience']
+            interviews['Consequentialist'] = interviews['Consequences']
+            interviews['Social'] = interviews[['Family', 'Community', 'Friends']].max(axis=1)
+            interviews = interviews.drop(['Experience', 'Consequences', 'Family', 'Community', 'Friends', 'Media', 'Laws', 'Holy Scripture'], axis=1)
+
         interviews = merge_codings(interviews)[MORALITY_ORIGIN]
         loss = pd.DataFrame([{mo:mean_squared_error(golden_labels[mo], interviews[mo]) for mo in MORALITY_ORIGIN}]) * weights
         loss['Model'] = model
         losses.append(loss)
 
     losses = pd.concat(losses, ignore_index=True).iloc[::-1]
-    losses['Model'] = losses['Model'].replace({'lg':'SpaCy (Labels)', 'bert':'BERT (Labels)', 'bart':'BART (Labels)', 'entail':'Entailment (Labels)', 'entail_explained':'Entailment (Labels+Notes)', 'top':'Entailment (Labels+Notes+Distro)', 'ml-top':'Entailment-ML (Labels+Notes+Distro)', 'baseline':'Naive (Distro)', 'chatgpt':'GPT-3.5 (Labels)'})
+    losses['Model'] = losses['Model'].replace({'lg':'SpaCy (Labels)', 'bert':'BERT (Labels)', 'bart':'BART (Labels)', 'entail_ml':'Entailment (Labels)', 'entail_ml_explained':'Entailment (Labels+Notes)', 'top':'Entailment (Labels+Notes+Distro)', 'chatgpt':'GPT-3.5 (Labels)'})
 
     #Plot model comparison
     sns.set(context='paper', style='white', color_codes=True, font_scale=2)
@@ -54,6 +48,7 @@ def plot_model_evaluation(codings, models, prefix):
     plt.axvline(x=coders_agreement, linestyle='-', linewidth=4, color='indianred', label='Coders Disagreement')
     plt.axvline(x=min_loss, linestyle='--', linewidth=1, color='grey')
     plt.xlabel('Normalized Mean Squared Error')
+    plt.ylabel('')
     plt.xticks([coders_agreement, min_loss], [str(round(coders_agreement, 2))[1:], str(round(min_loss, 2))[1:]])
     plt.title('Model Comparison')
     plt.legend(loc='upper right', bbox_to_anchor=(1.65, 1.03), fontsize='small')
@@ -76,9 +71,9 @@ def plot_coders_agreement(codings):
     sns.set(context='paper', style='white', color_codes=True, font_scale=4)
     plt.figure(figsize=(10, 10))
     ax = sns.heatmap(heatmap, cmap = sns.color_palette('PuBuGn', n_colors=6), vmin=-0.2, vmax=1)
-    plt.ylabel('Coder A')
-    plt.xlabel('Coder B')
-    plt.xticks(rotation=45, ha='right')
+    plt.ylabel('')
+    plt.xlabel('')
+    plt.xticks(rotation=35, ha='right')
     colorbar = ax.collections[0].colorbar
     colorbar.set_ticks([n/10 for n in range(-1, 10, 2)]) 
     colorbar.set_ticklabels(['Poor', 'Slight', 'Fair', 'Moderate', 'Substantial', 'Perfect'])
@@ -90,13 +85,12 @@ def plot_coders_agreement(codings):
 if __name__ == '__main__':
     #Hyperparameters
     config = [1,2]
-    models = ['lg', 'bert', 'bart', 'chatgpt', 'entail', 'entail_explained', 'top', 'ml-top']
-    prefix = 'sm-' if MERGE_MORALITY_ORIGINS else ''
-    codings = pd.read_pickle('data/cache/'+prefix+'morality_model-top.pkl')[['Wave', 'Interview Code']]
+    models = ['lg', 'bert', 'bart', 'chatgpt', 'entail_ml', 'entail_ml_explained', 'top']
+    codings = pd.read_pickle('data/cache/morality_model-top.pkl')[['Wave', 'Interview Code']]
     codings = merge_codings(codings)
 
     for c in config:
         if c == 1:
-            plot_model_evaluation(codings, models, prefix)
+            plot_model_evaluation(codings, models)
         elif c == 2:
             plot_coders_agreement(codings)

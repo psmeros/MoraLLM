d0b06ea3d0a71f7a0779d2989f126b307883a749
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2024-12-09 21:27:49 +0100

    Explained gpt prompts

--- src/helpers.py
+++ src/helpers.py
@@ -173,9 +173,22 @@ NEWLINE = '\n\n\n'
 
 CODERS = ['Leke', 'Tomas']
 
-CHATGPT_PROB_PROMPT = 'Estimate the independent probability (from 0.0 to 1.0) of the following text belonging individually to the categories: Intuitive, Consequentialist, Social, Theistic. Response in the form \"Category:Probability\".'
+CHATGPT_PROB_PROMPT = """Classify these interview excerpts into four categories of moral reasoning: Intuitive, Consequentialist, Social, and Theistic.
+Here is a detailed explanation of each category:
+Intuitive: Moral reasoning in this category is based on instinct, gut feelings, or innate senses of right and wrong.
+Consequentialist: This category evaluates the morality of actions based on their outcomes or consequences.
+Social: This category emphasizes the role of social norms, relationships, parents, friends, and communal well-being.
+Theistic: Moral reasoning in this category is guided by religious beliefs, divine principles, god, bible, commandments, or a sense of duty to a higher power.
+Task: When analyzing a given text, estimate the independent probability (from 0.0 to 1.0) of the four categories describing the moral reasoning expressed in the text. Response strictly in the form \"Category:Probability\"."""
 
-CHATGPT_BOOL_PROMPT = 'Estimate whether the following text belongs to the categories: Intuitive, Consequentialist, Social, Theistic. Response strictly in the form \"Category:True/False\".'
+CHATGPT_BOOL_PROMPT = """Classify these interview excerpts into four categories of moral reasoning: Intuitive, Consequentialist, Social, and Theistic.
+Here is a detailed explanation of each category:
+Intuitive: Moral reasoning in this category is based on instinct, gut feelings, or innate senses of right and wrong.
+Consequentialist: This category evaluates the morality of actions based on their outcomes or consequences.
+Social: This category emphasizes the role of social norms, relationships, parents, friends, and communal well-being.
+Theistic: Moral reasoning in this category is guided by religious beliefs, divine principles, god, bible, commandments, or a sense of duty to a higher power.
+Task: When analyzing a given text, estimate whether the four categories describe the moral reasoning expressed in the text. Response strictly in the form \"Category:True/False\"."""
+
 
 SURVEY_ATTRIBUTES = {'Wave 1':{'IDS':'Survey Id',
                                'PINCOME':'Household Income',

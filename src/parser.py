4a17917a490defc30f06433bbfecd3ae4996721a
Panayiotis Smeros <panayiotis.smeros@alumni.epfl.ch>
2025-01-22 18:23:36 +0100

    small bug in crowd labeling

--- src/parser.py
+++ src/parser.py
@@ -465,8 +465,8 @@ def compute_linguistics():
 
 def prepare_crowd_labeling(morality_text):
     interviews = prepare_data([], extend_dataset=True)
+    interviews[morality_text] = interviews[morality_text].str.replace('\n', '<br>').replace('', pd.NA).apply(lambda t: re.sub(r'M[24567]:', '', t) if pd.notna(t) else t)
     interviews = interviews[['Survey Id', morality_text]].dropna(subset=[morality_text]).reset_index(drop=True)
-    interviews[morality_text] = interviews[morality_text].str.replace('\n', '</br>').apply(lambda t: re.sub(r'M[24567]:', '', t))
     interviews.to_csv('data/crowd/' + morality_text + '.csv', index=False)
 
 if __name__ == '__main__':
